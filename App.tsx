23ec86e5ad807ae8b91f693eeaaa69acae02f1eb
Mohammad Al Amin <alamin.cse.99@gmail.com>
2026-01-16 03:38:12 +0600

    referral screen complete

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import RootNavigator from './src/navigator/RootNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import ReferralScreen from './src/screen/referral/ReferralScreen ';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <RootNavigator />
+    // </NavigationContainer>
+    <ReferralScreen />
   );
 };
 

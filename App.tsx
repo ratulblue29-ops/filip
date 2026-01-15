2290ef1a27766eb454561e02bb9856bea9b4e164
Mohammad Al Amin <alamin.cse.99@gmail.com>
2026-01-16 03:46:40 +0600

    MyAvailabilities screen structure ready

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import RootNavigator from './src/navigator/RootNavigator';
 import { NavigationContainer } from '@react-navigation/native';
-import ReferralScreen from './src/screen/referral/ReferralScreen ';
 
 const App = () => {
   return (
-    // <NavigationContainer>
-    //   <RootNavigator />
-    // </NavigationContainer>
-    <ReferralScreen />
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
   );
 };
 

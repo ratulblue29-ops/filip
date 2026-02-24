d5bcc6959e182b7272a29900319f7098a1865a4f
ratulblue29-ops <ratul.blue29@gmail.com>
2026-02-25 00:43:23 +0600

    google_signin_solved2

--- App.tsx
+++ App.tsx
@@ -1,80 +1,3 @@
-
-// import React, { useEffect } from 'react';
-// import RootNavigator from './src/navigator/RootNavigator';
-// import { NavigationContainer } from '@react-navigation/native';
-// import Toast from 'react-native-toast-message';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { GoogleSignin } from '@react-native-google-signin/google-signin';
-// import { LogBox } from 'react-native';
-
-// import { getApp } from '@react-native-firebase/app';
-// import {
-//   getMessaging,
-//   onMessage,
-// } from '@react-native-firebase/messaging';
-
-// import { registerFCMToken } from './src/services/FCMnotification';
-
-// // Ignore deprecated Firebase namespaced API warnings
-// LogBox.ignoreLogs([
-//   'This method is deprecated (as well as all React Native Firebase namespaced API)',
-// ]);
-
-// // Initialize React Query Client
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       retry: 1,
-//       refetchOnWindowFocus: false,
-//     },
-//   },
-// });
-
-// const App = () => {
-//   // Configure Google Sign-In
-//   useEffect(() => {
-//     GoogleSignin.configure({
-//       webClientId:
-//         '842815751322-fgg618jn2o3uldffnhaabi6k2se3kedm.apps.googleusercontent.com',
-//       offlineAccess: true,
-//     });
-//   }, []);
-
-//   // Register FCM token once
-//   useEffect(() => {
-//     registerFCMToken().catch(err => console.log('FCM Token Error:', err));
-//   }, []);
-
-//   // Listen to foreground notifications (Fixed Modular API)
-//   useEffect(() => {
-//     const app = getApp();
-//     const messaging = getMessaging(app);
-
-//     const unsubscribe = onMessage(messaging, async remoteMessage => {
-//       console.log('Foreground Notification:', remoteMessage);
-
-//       Toast.show({
-//         type: 'info',
-//         text1: remoteMessage.notification?.title ?? 'Notification',
-//         text2: remoteMessage.notification?.body ?? '',
-//       });
-//     });
-
-//     return unsubscribe;
-//   }, []);
-
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       <NavigationContainer>
-//         <RootNavigator />
-//       </NavigationContainer>
-
-//       <Toast />
-//     </QueryClientProvider>
-//   );
-// };
-
-// export default App;
 import React, { useEffect } from 'react';
 import RootNavigator from './src/navigator/RootNavigator';
 import { NavigationContainer } from '@react-navigation/native';
@@ -86,10 +9,7 @@ import { LogBox } from 'react-native';
 import { StripeProvider } from '@stripe/stripe-react-native';
 
 import { getApp } from '@react-native-firebase/app';
-import {
-  getMessaging,
-  onMessage,
-} from '@react-native-firebase/messaging';
+import { getMessaging, onMessage } from '@react-native-firebase/messaging';
 
 import { registerFCMToken } from './src/services/FCMnotification';
 
@@ -138,9 +58,7 @@ const App = () => {
   }, []);
 
   return (
-    <StripeProvider
-      publishableKey="pk_test_51SL7SOLT7u05bl0T7ycIXtQ087oy07qqZxzYrTMR1JjpxGrs8jVcKRtZCte98moBU053lCoqt8aWoXiCT5iNUaRn00AGBjmxrR"
-    >
+    <StripeProvider publishableKey="pk_test_51SL7SOLT7u05bl0T7ycIXtQ087oy07qqZxzYrTMR1JjpxGrs8jVcKRtZCte98moBU053lCoqt8aWoXiCT5iNUaRn00AGBjmxrR">
       <QueryClientProvider client={queryClient}>
         <NavigationContainer>
           <RootNavigator />
